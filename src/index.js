671b9fc69565f440d39d046fd7c6c4b5bb033902
leolanese <developer@leolanese.com>
2021-01-01 21:51:27 +0000

    code improvements

--- src/index.js
+++ src/index.js
@@ -1,32 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './components/App'
-import reportWebVitals from './reportWebVitals'
-import axios from 'axios'
-import reducers from './reducers'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
+import axios from "axios";
+import reducers from "./reducers";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
 // store and passing the reducers
-const store = createStore(reducers)
+const store = createStore(reducers);
 
-const protocol = 'http';
-const domain = 'rem-rest-apo.herokuapp.com';
-const pathApi = '/api';
-const pathUsers = '/users'
+const protocol = "http";
+const domain = "rem-rest-apo.herokuapp.com";
+const pathApi = "/api";
+const pathUsers = "/users";
 const url = protocol + domain + pathApi;
 
-axios.defaults
-  .withCredentials = true;
-axios.defaults
-  .baseURL = url;
-
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = url;
 
 ReactDOM.render(
   // <React.StrictMode><App /></React.StrictMode>,
-  <Provider store={store}><App /></Provider>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
